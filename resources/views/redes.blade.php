9bad03e16d487563cdf97bd68cf6dc1aa82c6f71
GeneralGilmar <gaspar.general1987@gmail.com>
2021-08-20 22:12:04 +0100

    feito

--- resources/views/redes.blade.php
+++ resources/views/redes.blade.php
@@ -7,7 +7,7 @@
 <main id="main">
     <!-- ======= About Section ======= -->
     <section id="services" class="about">
-
+ 
       <div class="container" data-aos="fade-up">
         <div class="row gx-0">
 
@@ -15,7 +15,7 @@
             <div class="content">
               <h2>Serviço de redes e Manutenção</h2>
               <p>
-              O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, 
+              O Lorem Ipsum é um texto modelo da dhydud indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, 
               que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.
               </p>
             </div>
